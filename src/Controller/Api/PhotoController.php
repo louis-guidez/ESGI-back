99d406eb8f4bfcb3433e43b619dcc231302cea70
louis-guidez <louisguidez3@gmail.com>
2025-06-07 19:04:43 +0200

    ft: CRUD des entitées

--- src/Controller/Api/PhotoController.php
+++ src/Controller/Api/PhotoController.php
@@ -1,12 +1,15 @@
 <?php
 
-// src/Controller/Api/UserController.php
+// src/Controller/Api/PhotoController.php
 
 namespace App\Controller\Api;
 
+use App\Entity\Photo;
 use App\Repository\PhotoRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PhotoController extends AbstractController
@@ -27,5 +30,46 @@ class PhotoController extends AbstractController
         }
 
         return $this->json($data);
+    }
+
+    #[Route('/api/photos', name: 'api_photos_new', methods: ['POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $photo = new Photo();
+        $photo->setUrlChemin($data['urlChemin'] ?? null);
+        if (isset($data['dateUpload'])) {
+            $photo->setDateUpload(new \DateTime($data['dateUpload']));
+        }
+
+        $entityManager->persist($photo);
+        $entityManager->flush();
+
+        return $this->json(['id' => $photo->getId()], 201);
+    }
+
+    #[Route('/api/photos/{id}', name: 'api_photos_edit', methods: ['PUT'])]
+    public function edit(Request $request, EntityManagerInterface $entityManager, Photo $photo): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $photo->setUrlChemin($data['urlChemin'] ?? null);
+        if (isset($data['dateUpload'])) {
+            $photo->setDateUpload(new \DateTime($data['dateUpload']));
+        }
+
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Photo updated']);
+    }
+
+    #[Route('/api/photos/{id}', name: 'api_photos_delete', methods: ['DELETE'])]
+    public function delete(EntityManagerInterface $entityManager, Photo $photo): JsonResponse
+    {
+        $entityManager->remove($photo);
+        $entityManager->flush();
+
+        return $this->json(['status' => 'Photo deleted']);
     }
 }
